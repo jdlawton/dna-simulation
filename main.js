fac4fc186e1dd54d2e02d87b3c4f81bca5babeb8
jdlawton <joe.lawton@outlook.com>
2021-01-07 11:24:53 -0600

    Created factory function for sumulating pAequor. Created mutate function to simulate a mutating DNA base and compareDNA function to compare the DNA of two organisms and report the % similar.

--- main.js
+++ main.js
@@ -13,6 +13,49 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (number, baseArray) => {
+  return {
+    specimenNum: number,
+    dna: baseArray,
+    mutate() {
+      const baseToMutate = Math.floor(Math.random() * 15);
+      console.log("Mutating Base #: " + baseToMutate);
+      let newBase = '';
+      do {
+        newBase = returnRandBase();
+        console.log("inside do while");
+      }
+      while (this.dna[baseToMutate] === newBase);
+      console.log ("Original Base: " + this.dna[baseToMutate]);
+      console.log ("New Base: " + newBase);
+      this.dna[baseToMutate] = newBase;
+    }, 
+    compareDNA(pAequor2) {
+      let numMatch = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === pAequor2.dna[i]){
+          numMatch ++;
+        }
+      }
+      const percentMatch = (numMatch / this.dna.length) * 100;
+      console.log(`${this.specimenNum} and ${pAequor2.specimenNum} have ${percentMatch.toFixed(2)}% DNA in common.`)
+    },
+  };
+}
+
+//test factory function
+let testObj1 = pAequorFactory(1, mockUpStrand());
+let testObj2 = pAequorFactory(2, mockUpStrand());
+//testObj.mutate();
+/*
+console.log ("Starting Test Obj");
+console.log(testObj);
+testObj.mutate();
+console.log("=============");
+console.log ("Mutated Test Obj");
+console.log(testObj);
+*/
+testObj1.compareDNA(testObj2);
 
 
 
